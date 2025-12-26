5535fdb4f2830bb9810d3e1cc53af560d2abb60c
cjhueck <c.hueck@yahoo.de>
2025-12-26 19:37:44 +0100

    new

--- temp_check.py
+++
@@ -1,8 +0,0 @@
-import json
-d = json.load(open('page-break-markers.json', 'r', encoding='utf-8'))
-breaks = d.get('GA153', {}).get('breaks', [])
-for b in breaks:
-    page = b.get('page', 0)
-    if page and page <= 30:
-        right = (b.get('right') or '')[:100]
-        print(f"S.{page}: {right}...")
