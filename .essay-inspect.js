20a1657001c0fa1d628f3efed5a8d3822e05e052
cjhueck <c.hueck@yahoo.de>
2026-06-16 23:12:56 +0200

    new

--- .essay-inspect.js
+++
@@ -1,8 +0,0 @@
-let s = require('fs').readFileSync('.essay-test-response.json', 'utf8');
-if (s.charCodeAt(0) === 0xFEFF) s = s.slice(1);
-const r = JSON.parse(s);
-require('fs').writeFileSync('.essay-test-content.md', r.content, 'utf8');
-console.log('Content gespeichert. Sources (' + r.sources.length + '):');
-r.sources.forEach((src, i) => {
-  console.log('  [' + (i + 1) + '] ' + src.ID + ':' + src.index + '  score=' + src.score + '  --  ' + (src.fileName || src.title).substring(0, 80));
-});
